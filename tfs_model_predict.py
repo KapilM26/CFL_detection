f2919eefa4aab3f2efea512eb281c31408931f7e
Kartik-Nair <kartiknair1999@gmail.com>
2024-02-27 19:02:38 -0500

    input image removed normalization

--- tfs_model_predict.py
+++ tfs_model_predict.py
@@ -9,8 +9,10 @@ import matplotlib.pyplot as plt
 image_path = "C:/Users/kgrna/Downloads/Kartik/Kartik/ELG5902/CFL_training_data-20240207T044725Z-001/CFL_training_data/input/VesselD20H15_0.jpg"
 
 image = cv2.resize(cv2.imread(image_path), (256,256))
-image_array = np.array(image).reshape(1, 256, 256, 3) / 255.0
+image_array = np.array(image)#.reshape(1, 256, 256, 3) / 255.0
 
+# image_array = np.array(image) / 255.0
+image_array=np.expand_dims(image_array,axis=0)
 
 # image = Image.open(image_path)
 print(np.array(image_array).shape)
@@ -29,15 +31,17 @@ response = requests.post(url, data=data, headers=headers)
 
 # Assuming the response contains the image data in a 'predictions' field
 predictions = response.json()['predictions']
-
+predictions_array = np.array(predictions)
+print(predictions_array[predictions_array>=0.5])
+# print(predictions_array[0].shape)
+# predictions_array[predictions_array>=0.5] = 1
+# predictions_array[predictions_array<0.5] = 0
+# print(predictions_array)
+predictions_array = predictions_array.reshape(256,256)
 # Convert predictions to a NumPy array
-image_array = np.array(predictions).reshape(256,256)
-print(image_array * 255)
-# np.save("predictions2.npy", image_array)
+# image_array = np.array(predictions).reshape(256,256,1)
 
-# # # Convert the NumPy array to an image
-# image = Image.fromarray(image_array[0], mode="RGB")
-image = Image.fromarray((image_array * 255).astype(np.uint8))
+# image = Image.fromarray((predictions_array[0] * 255).astype(np.uint8))
 
 # # image.save("output_image.jpg")
 
@@ -55,15 +59,22 @@ image = Image.fromarray((image_array * 255).astype(np.uint8))
 # plt.grid(True)
 # plt.show()
 
-plt.imshow(image_array, cmap='gray')  # 'gray' colormap for grayscale images
+plt.imshow(predictions_array, cmap='gray')  # 'gray' colormap for grayscale images
+# plt.imshow(image_array[0], cmap='gray')  # 'gray' colormap for grayscale images
 
-# Add colorbar for reference
+# # Add colorbar for reference
 plt.colorbar()
 
-# Customize the plot
-plt.title('2D Array Visualization')
-plt.xlabel('Column Index')
-plt.ylabel('Row Index')
+# # Customize the plot
+# plt.title('2D Array Visualization')
+# plt.xlabel('Column Index')
+# plt.ylabel('Row Index')
 
-# Display the plot
+# # Display the plot
 plt.show()
+
+
+
+
+
+
